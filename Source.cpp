133958ad30b78dd663fdc1b6eb00576026eef8e3
NG240 <NG240@LAPTOP-JSBSN1US>
2022-02-12 15:27:55 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void main() //простая программа
+{
+	int num;
+	num = 1;
+	cout << "menj zovut Ivanov Ivan.\n J na " << num << " kurse.";
+	cout << endl;
+}
