1cf89cbf9796b48d8886424574c041f78d908fc7
Polich <58328813+polina1441@users.noreply.github.com>
2022-02-12 16:49:53 +0300

    Delete Source.cpp

--- Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-void main() //простая программа
-{
-	int num;
-	num = 1;
-	cout << "menj zovut Ivanov Ivan.\n J na " << num << " kurse.";
-	cout << endl;
-}
